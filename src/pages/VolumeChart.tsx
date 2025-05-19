806e48c9f08be2fa6f95bba588a4b89069dea923
Kaushik Sivakumar <kaushiksivakumar@Kaushiks-MacBook-Air.local>
2025-05-18 23:01:29 -0400

    header

--- src/pages/VolumeChart.tsx
+++ src/pages/VolumeChart.tsx
@@ -1,6 +1,7 @@
 // src/pages/VolumeChart.tsx
 
 import React, { useEffect, useState } from "react";
+import Header from "../components/Header";
 import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
